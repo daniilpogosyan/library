dc6155ae4d1006f08e23551b87a452a0ca063393
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-04-01 22:37:08 +0500

    Add logic, fix typos
    I had to somehow bind book objects in array with DOM element in html,
    so I used data-id to do it.
    In `createDOMBook` I got to create a whole book block. I didn't
    want to create by element using `appendChild` bunch of times,
    so providing that structure of the book will be the same for every
    instance, I just put it in innerHTML.

---
+++ script.js
@@ -0,0 +1,106 @@
+function Book(title ='dabook', author, pageCount, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pageCount = pageCount;
+  this.isRead = isRead;
+}
+
+Book.prototype.info = function() {
+  const getReadStatus = (isRead) => {
+    if (isRead)   return 'read already';
+    return 'not read yet';
+  }
+  const isReadStr = getReadStatus(this.isRead);
+
+  return `${this.title}, by ${this.author}, ${this.pageCount} pages, ${isReadStr}`;
+}
+
+
+const booksDB = [];
+
+const showNewBookFormBtn = document.querySelector('.add-book__button');
+showNewBookFormBtn.addEventListener('click', showNewBookForm);
+
+const hideNewBookFormBtn = document.querySelector('.new-book-form__close-form-btn');
+hideNewBookFormBtn.addEventListener('click', hideNewBookForm);
+
+
+const deleteBookBtns = document.querySelectorAll(".button__delete");
+const addBookBtn = document.querySelector(".new-book-form__add-btn");
+addBookBtn.addEventListener('click', () => {
+  const book = getBookfromForm();
+  addBookToDB(book, booksDB);
+  addBookToDOM(book);
+  hideNewBookForm();
+})
+
+function getBookfromForm() {
+  const form = document.querySelector('.new-book-form');
+  const inputs = form.querySelectorAll('.new-book-form__input');
+  const book = new Book();
+  inputs.forEach((input) => {
+    book[input.getAttribute("name")] = input.value;
+  });
+  book.id = Math.floor((Math.random() * 100000));
+  return book;
+}
+function addBookToDB(book, DB) {
+  DB.push(book);
+}
+
+function setBtnsListenerstoBook(bookId) {
+  deleteBtn = document.querySelector(`.button__delete[data-id="${bookId}"]`).addEventListener(
+    'click', () => {
+      deleteBookFromDOM(bookId);
+      deleteBookFromDB(bookId);
+    });
+}
+function createDOMBook (book) {
+  const DOMBook = document.createElement("li");
+  DOMBook.classList.add("book-wrapper");
+  DOMBook.dataset.id = book.id;
+  DOMBook.innerHTML = `
+          <article class="book">
+            <div class="book__buttons">
+              <button data-id="${book.id}" class="book__button button__edit"></button>
+              <button data-id="${book.id}" class="book__button button__delete"></button>
+            </div>
+            <div class="book__info">
+              <h2 class="book__title">${book.title}</h2>
+              <p class="book__author">${book.author}</p>
+              <p class="book__page-count">${book.pageCount}</p>
+            </div>
+            <p class="book__isread">${book.isRead}</p>
+          </article>`;
+  return DOMBook;
+}
+function displayBooksDB(booksDB) {
+  const bookList = document.querySelector(".book-list");
+  booksDB.forEach((book) => addDOMBookToList(book));
+}
+function addDOMBookToList(book) {
+  const DOMBook = createDOMBook(book);
+  document.querySelector(".book-list").appendChild(DOMBook);
+  setBtnsListenerstoBook(DOMBook.dataset.id);
+}
+
+function deleteBookFromDB(bookId) {
+  const index = booksDB.findIndex((book) => {
+    return book.id == bookId;
+  });
+  booksDB.splice(index, 1);
+}
+
+function deleteBookFromDOM(bookId) {
+    const book = document.querySelector(`.book-wrapper[data-id="${bookId}`)
+    book.remove();  
+}
+
+
+function hideNewBookForm() {
+  document.querySelector('.new-book-window').classList.remove('new-book-window--isshown');
+}
+
+function showNewBookForm() {
+  document.querySelector('.new-book-window').classList.add('new-book-window--isshown');
+}
